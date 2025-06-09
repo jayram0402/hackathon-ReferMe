a911b307a2a597a7988d7db1d6f272fd590614bf
Jayram Sharma <147648366+jayram0402@users.noreply.github.com>
2025-06-09 17:41:24 +0530

    Update Hero.jsx

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <h1>
               <b>Referral Network..!</b>
             </h1>
-            <p>Connecting Professional, Streamlining Referrals </p>
+            <p>Connecting Professionally, Streamlining Referrals </p>
             <JoinNow />
           </div>
         </div>
